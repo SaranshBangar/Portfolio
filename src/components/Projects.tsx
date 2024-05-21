1e6ddcf3c8b8d5eaa66648efa73b654c51d8f57e
Saransh Bangar <saranshbangad@gmail.com>
2024-05-21 14:01:18 +0530

    Added new projects

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,18 +2,14 @@ import { DirectionAwareHover } from "./ui/direction-aware-hover"
 import { FaGithub } from "react-icons/fa";
 
 const Projects = () => {
-  // const imageUrl1 = "/car_image.jpg";
-  // const imageUrl2 = "/nike_image.jpg";
-  // const imageUrl3 = "/forum_image.jpg";
-  // const imageUrl4 = "/blog_image.jpg";
-  const imageUrl1 = "/trie.png";
-  const imageUrl2 = "/gfg.png";
-  const imageUrl3 = "/car_image.jpg";
-  const imageUrl4 = "/forum_image.jpg";
+  const imageUrl1 = "/car_image.jpg";
+  const imageUrl2 = "/nike_image.jpg";
+  const imageUrl3 = "/forum_image.jpg";
+  const imageUrl4 = "/blog_image.jpg";
   return (
     <section className="-mt-16 max-sm:-mt-12 w-[52%]">
         <div className="relative flex items-center justify-evenly flex-wrap mb-6 gap-5">
-            {/* <a href="https://car-rental-next-js-ztra.vercel.app/" target="_blank">
+            <a href="https://car-rental-next-js-ztra.vercel.app/" target="_blank">
               <DirectionAwareHover imageUrl={imageUrl1}>
                 <p className="font-bold text-xl">
                   <a href="https://github.com/SaranshBangar/Car_Rental_NextJS" target="_blank">
@@ -57,54 +53,6 @@ const Projects = () => {
                   </a>
                 </p>
                 <a href="https://github.com/SaranshBangar/Blog-Website-NextJS" target='_blank'>
-                    <FaGithub className='w-[22px] h-[22px] max-sm:w-[18px] max-sm:h-[18px] mt-2' />
-                </a>
-              </DirectionAwareHover>
-            </a> */}
-            <a href="https://car-rental-next-js-ztra.vercel.app/" target="_blank">
-              <DirectionAwareHover imageUrl={imageUrl1}>
-                <p className="font-bold text-xl">
-                  <a href="https://github.com/SaranshBangar/Car_Rental_NextJS" target="_blank">
-                    Usage of Trie - C++
-                  </a>
-                </p>
-                <a href="https://github.com/SaranshBangar/Car_Rental_NextJS" target='_blank'>
-                    <FaGithub className='w-[22px] h-[22px] max-sm:w-[18px] max-sm:h-[18px] mt-2' />
-                </a>
-              </DirectionAwareHover>
-            </a>
-            <a href="https://github.com/SaranshBangar/GeeksforGeeks-Solutions" target="_blank">
-              <DirectionAwareHover imageUrl={imageUrl2}>
-                <p className="font-bold text-xl">
-                  <a href="https://github.com/SaranshBangar/GeeksforGeeks-Solutions" target="_blank">
-                    GFG Solutions - C++
-                  </a>
-                </p>
-                <a href="https://github.com/SaranshBangar/GeeksforGeeks-Solutions" target='_blank'>
-                    <FaGithub className='w-[22px] h-[22px] max-sm:w-[18px] max-sm:h-[18px] mt-2' />
-                </a>
-              </DirectionAwareHover>
-            </a>
-            <a href="https://car-rental-next-js-ztra.vercel.app/" target="_blank">
-              <DirectionAwareHover imageUrl={imageUrl3}>
-                <p className="font-bold text-xl">
-                  <a href="https://github.com/SaranshBangar/Car_Rental_NextJS" target="_blank">
-                    Car Rental Website - Next.js
-                  </a>
-                </p>
-                <a href="https://github.com/SaranshBangar/Car_Rental_NextJS" target='_blank'>
-                    <FaGithub className='w-[22px] h-[22px] max-sm:w-[18px] max-sm:h-[18px] mt-2' />
-                </a>
-              </DirectionAwareHover>
-            </a>
-            <a href="https://ideaclinic-forum.vercel.app/" target="_blank">
-              <DirectionAwareHover imageUrl={imageUrl4}>
-                <p className="font-bold text-xl">
-                  <a href="https://github.com/SaranshBangar/ideaclinic_forum" target="_blank">
-                    IdeaClinic Website - Next.js
-                  </a>
-                </p>
-                <a href="https://github.com/SaranshBangar/ideaclinic_forum" target='_blank'>
                     <FaGithub className='w-[22px] h-[22px] max-sm:w-[18px] max-sm:h-[18px] mt-2' />
                 </a>
               </DirectionAwareHover>
