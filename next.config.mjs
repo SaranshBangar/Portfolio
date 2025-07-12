d9a4527ee8138391df009574475aced725b80b57
Saransh Bangar <saranshbangad@gmail.com>
2025-07-12 15:13:53 +0530

    style: format image domains array for improved readability

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "res.cloudinary.com", "www.google.com", "api.microlink.io"],
+    domains: [
+      "images.unsplash.com",
+      "res.cloudinary.com",
+      "www.google.com",
+      "api.microlink.io",
+    ],
   },
   typescript: {
     ignoreBuildErrors: true,
